c7a831dcbcea5fe3007e6d0941a5efaa2a481597
Allison Bohannon <akboh.github@gmail.com>
2022-11-20 21:56:43 -0800

    commit tree e47547ded63c440d8571a835a962cf4d9e059059

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
   get '/games' do
     games = Game.all.order(:title).limit(10)
+    games.to_json
+  end
+
+  get '/reviews' do
+    games = Review.all.order(:title).limit(10)
     games.to_json
   end
 
@@ -14,6 +21,36 @@ class ApplicationController < Sinatra::Base
         user: { only: [:name] }
       } }
     })
+  end
+
+  get '/reviews/:id' do
+    game = Review.find(params[:id])
+    game.to_json
+  end
+
+  delete '/reviews/:id' do 
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+    
+  patch "/reviews/:id" do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment],
+    )
+    review.to_json
   end
 
 end
